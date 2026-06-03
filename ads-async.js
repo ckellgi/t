94115ba06eada4db19d77f5925624c9da50adfac
ckellgi <ykurdyaev@gmail.com>
2026-06-03 18:32:48 +0600

    Delete ads-async.js

--- ads-async.js
+++
@@ -1,5 +0,0 @@
-(function() {
-    var script = document.createElement('script');
-    script.src = 'https://r.mradx.net/img/29/F207CB.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
-})();
